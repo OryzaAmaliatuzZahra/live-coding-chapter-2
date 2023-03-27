4980d2823ba4c36c307f17c95f36648ae647a116
OryzaAZ <zoryzaamaliatuz@gmail.com>
2023-03-27 13:49:32 +0700

    live coding chapter 2 commit

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,5 @@
 for (let i = 0; i<person.length; i++) {
-//     if(person[i].isActive === true && person[i].gender == "female") {
-//         console.log(person[i])
-//     }
-// }
+    if(person[i].isActive === true && person[i].gender == "female") {
+        console.log(person[i])
+    }
+}
